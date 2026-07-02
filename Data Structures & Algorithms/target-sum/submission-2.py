e9af278d1823008dca7306cb08a2ad5dfa5a8d7a
Weijian Liu <vj.liu@outlook.com>
2026-07-02 15:38:24 +0800

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        total_sum = sum(nums)
+
+        if abs(target) > total_sum:
+            return 0
+
+        #P + N = target
+        #P - N = total_sum
+        #P = (target + total_sum) // 2
+
+        if (target + total_sum) % 2 != 0:
+            return 0
+
+        positive_target = (target + total_sum) // 2
+        n = len(nums)
+
+        dp = [[0] * (positive_target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(positive_target + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j >= nums[i - 1]:
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]]
+        
+        return dp[n][positive_target]
